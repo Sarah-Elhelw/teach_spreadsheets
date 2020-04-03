3fb3ae4e915a8ea9aaa6dcc178de5d55229d18e5
EpicM <santangelomike99@gmail.com>
2020-04-03 21:05:59 +0200

    maven at root

---
+++ src/main/java/io/github/oliviercailloux/teach_spreadsheets/App.java
@@ -0,0 +1,17 @@
+package io.github.oliviercailloux.teach_spreadsheets;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class App {
+	@SuppressWarnings("unused")
+	private static final Logger LOGGER = LoggerFactory.getLogger(App.class);
+
+	public static void main(String[] args) throws Exception {
+		new App().proceed();
+	}
+
+	public void proceed() {
+		LOGGER.info("Hello World!");
+	}
+}
