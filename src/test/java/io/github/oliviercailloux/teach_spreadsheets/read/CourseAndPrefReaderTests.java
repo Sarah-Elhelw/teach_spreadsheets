dc40503203affe2da649fb0b1dc41d2bbcc35227
Sarah-Elhelw <sarah_elhelw@hotmail.fr>
2020-04-12 15:13:23 +0200

    Adding the file for testing CourseAndPrefReader

---
+++ src/test/java/io/github/oliviercailloux/teach_spreadsheets/read/CourseAndPrefReaderTests.java
@@ -0,0 +1,16 @@
+package io.github.oliviercailloux.teach_spreadsheets.read;
+
+import org.junit.jupiter.api.Test;
+
+public class CourseAndPrefReaderTests {
+	
+	@Test
+	void testSetInfoPref() {
+		
+	}
+	
+	@Test
+	void testSetInfoCourse() {
+		
+	}
+}
