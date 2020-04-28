4eaf300600da195cbbfc7680f18881ac23f5662c
EpicM <santangelomike99@gmail.com>
2020-04-28 21:05:04 +0200

    junit tests

--- src/test/java/io/github/oliviercailloux/teach_spreadsheets/base/CalcDataTests.java
+++ src/test/java/io/github/oliviercailloux/teach_spreadsheets/base/CalcDataTests.java
@@ -1,6 +1,7 @@
 package io.github.oliviercailloux.teach_spreadsheets.base;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
 
@@ -16,7 +17,6 @@ public class CalcDataTests {
 	@Test
 	void testNewInstance() {
 		Teacher.Builder teacherBuilder = Teacher.Builder.newInstance();
-		teacherBuilder.setAddress("Pont du maréchal de lattre de tassigny");
 		teacherBuilder.setLastName("Doe");
 		Teacher teacher = teacherBuilder.build();
 
@@ -32,11 +32,11 @@ public class CalcDataTests {
 
 		CoursePref coursePref1 = coursePrefBuilder.build();
 
-		courseBuilder.setCountGroupsCM(10);
-		courseBuilder.setnbMinutesCM(20);
+		courseBuilder.setCountGroupsCM(3);
+		courseBuilder.setnbMinutesCM(2);
 		courseBuilder.setName("Java");
-		courseBuilder.setStudyYear("2012");
-		courseBuilder.setSemester(1);
+		courseBuilder.setStudyYear("2015");
+		courseBuilder.setSemester(2);
 
 		coursePrefBuilder = CoursePref.Builder.newInstance(courseBuilder.build(), teacher);
 
@@ -46,11 +46,10 @@ public class CalcDataTests {
 
 		ImmutableSet<CoursePref> coursePrefs = ImmutableSet.copyOf(new CoursePref[] { coursePref1, coursePref2 });
 
-		CalcData calcData = CalcData.newInstance(coursePrefs, teacher);
-		String actual = calcData.toString();
-
-		String expected = "CalcData{coursePrefs=[CoursePref{prefCM=A, prefTD=UNSPECIFIED, prefCMTD=UNSPECIFIED, prefTP=UNSPECIFIED, prefCMTP=UNSPECIFIED, prefNbGroupsCM=0, prefNbGroupsTD=0, prefNbGroupsCMTD=0, prefNbGroupsTP=0, prefNbGroupsCMTP=0, Course=Course{name=Analyse de données, countGroupsTD=0, countGroupsCMTD=0, countGroupsTP=0, countGroupsCMTP=0, countGroupsCM=10, nbMinutesTD=0, nbMinutesCMTD=0, nbMinutesTP=0, nbMinutesCMTP=0, nbMinutesCM=20, studyYear=2012, semester=1}, Teacher=Teacher{lastName=Doe, firstName=, address=Pont du maréchal de lattre de tassigny, postCode=, city=, personalPhone=, mobilePhone=, personalEmail=, dauphineEmail=, status=, dauphinePhoneNumber=, office=}}, CoursePref{prefCM=A, prefTD=UNSPECIFIED, prefCMTD=UNSPECIFIED, prefTP=UNSPECIFIED, prefCMTP=UNSPECIFIED, prefNbGroupsCM=0, prefNbGroupsTD=0, prefNbGroupsCMTD=0, prefNbGroupsTP=0, prefNbGroupsCMTP=0, Course=Course{name=Java, countGroupsTD=0, countGroupsCMTD=0, countGroupsTP=0, countGroupsCMTP=0, countGroupsCM=10, nbMinutesTD=0, nbMinutesCMTD=0, nbMinutesTP=0, nbMinutesCMTP=0, nbMinutesCM=20, studyYear=2012, semester=1}, Teacher=Teacher{lastName=Doe, firstName=, address=Pont du maréchal de lattre de tassigny, postCode=, city=, personalPhone=, mobilePhone=, personalEmail=, dauphineEmail=, status=, dauphinePhoneNumber=, office=}}], teacher=Teacher{lastName=Doe, firstName=, address=Pont du maréchal de lattre de tassigny, postCode=, city=, personalPhone=, mobilePhone=, personalEmail=, dauphineEmail=, status=, dauphinePhoneNumber=, office=}}";
-		assertEquals(expected, actual);
+		assertThrows(IllegalArgumentException.class, () -> {
+			CalcData.newInstance(coursePrefs, teacher);
+		});
+		
 	}
 
 	@Test
