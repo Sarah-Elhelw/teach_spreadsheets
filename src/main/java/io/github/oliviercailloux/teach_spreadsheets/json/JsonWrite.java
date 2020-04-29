5733b51af482024992a395c4949f8dc10c9496f1
EpicM <santangelomike99@gmail.com>
2020-04-29 11:17:39 +0200

    junit test write courses in a json file

--- src/main/java/io/github/oliviercailloux/teach_spreadsheets/json/JsonWrite.java
+++ src/main/java/io/github/oliviercailloux/teach_spreadsheets/json/JsonWrite.java
@@ -3,11 +3,10 @@ package io.github.oliviercailloux.teach_spreadsheets.json;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Set;
 
 import javax.json.bind.Jsonb;
 import javax.json.bind.JsonbBuilder;
-
-import com.google.common.collect.ImmutableSet;
 
 import io.github.oliviercailloux.teach_spreadsheets.base.Course;
 
@@ -15,7 +14,14 @@ import io.github.oliviercailloux.teach_spreadsheets.base.Course;
 import static com.google.common.base.Preconditions.checkNotNull;
 
 public class JsonWrite {
-	public static void writeCoursesInAJsonFile(Path filePath, ImmutableSet<Course> courses) throws Exception {
+	
+	/**
+	 * Serializes a Set of Course objects and writes it into a file
+	 * @param filePath the file path where we store the serialized courses
+	 * @param courses a collection of Course objects with no duplicate elements
+	 * @throws Exception
+	 */
+	public static void writeCoursesInAJsonFile(Path filePath, Set<Course> courses) throws Exception {
 		checkNotNull(filePath);
 		checkNotNull(courses);
 		
--- src/test/java/io/github/oliviercailloux/teach_spreadsheets/json/JsonWriteTests.java
+++ src/test/java/io/github/oliviercailloux/teach_spreadsheets/json/JsonWriteTests.java
@@ -1,13 +1,43 @@
 package io.github.oliviercailloux.teach_spreadsheets.json;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.HashSet;
+
+import javax.json.bind.Jsonb;
+import javax.json.bind.JsonbBuilder;
 
 import org.junit.jupiter.api.Test;
 
+import com.google.common.collect.ImmutableSet;
+
+import io.github.oliviercailloux.teach_spreadsheets.base.CalcData;
+import io.github.oliviercailloux.teach_spreadsheets.base.Course;
+import io.github.oliviercailloux.teach_spreadsheets.base.CoursePref;
+
 public class JsonWriteTests {
 	@Test
-	void testWriteCoursesInAJsonFile() {
+	void testWriteCoursesInAJsonFile() throws Exception {
+		final Path infile = Path.of(
+				"src/test/resources/io/github/oliviercailloux/teach_spreadsheets/read/Saisie_des_voeux_format simple.ods");
+		CalcData calcData = CalcData.getData(infile);
 		
+		ImmutableSet<CoursePref> coursePrefs = calcData.getCoursePrefs();
+		HashSet<Course> courses = new HashSet<>();
+		for (CoursePref coursePref : coursePrefs) {
+			courses.add(coursePref.getCourse());
+		}
+		
+		Path jsonPath = Path.of("courses.json");
+		JsonWrite.writeCoursesInAJsonFile(jsonPath, courses);
+		
+		try (Jsonb jsonb = JsonbBuilder.create()) {
+			String expected = jsonb.toJson(courses.toArray());
+			String actual = Files.readString(jsonPath, StandardCharsets.UTF_8);
+			assertEquals(expected, actual);
+		}
 	}
 }
