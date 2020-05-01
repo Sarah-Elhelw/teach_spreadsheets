d3e1cd27994289c37cbb52b8a9e12e19bc0b0477
EpicM <santangelomike99@gmail.com>
2020-05-01 19:50:51 +0200

    Revert "rewriting tests JsonWrite"
    This reverts commit 7b8e80aba8b09e6b29ff0cce302e0ffa16facd82.

--- src/test/java/io/github/oliviercailloux/teach_spreadsheets/json/JsonWriteTests.java
+++ src/test/java/io/github/oliviercailloux/teach_spreadsheets/json/JsonWriteTests.java
@@ -7,33 +7,41 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.FileSystem;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Set;
+import java.util.HashSet;
 
 import javax.json.bind.Jsonb;
 import javax.json.bind.JsonbBuilder;
 
 import org.junit.jupiter.api.Test;
 
+import com.google.common.collect.ImmutableSet;
 import com.google.common.jimfs.Configuration;
 import com.google.common.jimfs.Jimfs;
 
+import io.github.oliviercailloux.teach_spreadsheets.base.CalcData;
 import io.github.oliviercailloux.teach_spreadsheets.base.Course;
+import io.github.oliviercailloux.teach_spreadsheets.base.CoursePref;
+import io.github.oliviercailloux.teach_spreadsheets.read.CalcDataInitializerTests;
 
 public class JsonWriteTests {
-	
 	@Test
 	void testWriteCoursesInAJsonFile() throws Exception {
-		URL url = JsonWriteTests.class.getResource("coursesJsonWrite.json");
+		URL url = CalcDataInitializerTests.class.getResource("Saisie_des_voeux_format simple.ods");
 		final Path infile = Path.of(url.toURI());
+		CalcData calcData = CalcData.getData(infile);
 		
-		try (Jsonb jsonb = JsonbBuilder.create()) {
-			Set<Course> deserialized = jsonb.fromJson(Files.newInputStream(infile), Set.class);
-					
-			try (FileSystem inMemoryFs = Jimfs.newFileSystem(Configuration.unix())) {
-				Path jsonPath = inMemoryFs.getPath("courses.json");
-				JsonWrite.writeCoursesInAJsonFile(jsonPath, deserialized);	
-				
-				String expected = jsonb.toJson(deserialized.toArray());
+		ImmutableSet<CoursePref> coursePrefs = calcData.getCoursePrefs();
+		HashSet<Course> courses = new HashSet<>();
+		for (CoursePref coursePref : coursePrefs) {
+			courses.add(coursePref.getCourse());
+		}
+		
+		try (FileSystem inMemoryFs = Jimfs.newFileSystem(Configuration.unix())) {
+			Path jsonPath = inMemoryFs.getPath("courses.json");
+			JsonWrite.writeCoursesInAJsonFile(jsonPath, courses);
+			
+			try (Jsonb jsonb = JsonbBuilder.create()) {
+				String expected = jsonb.toJson(courses.toArray());
 				String actual = Files.readString(jsonPath, StandardCharsets.UTF_8);
 				assertEquals(expected, actual);
 			}
