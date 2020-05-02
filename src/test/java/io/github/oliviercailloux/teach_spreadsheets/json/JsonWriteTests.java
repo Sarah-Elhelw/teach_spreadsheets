07d35e20312884cad8a09428f83f9dd9fb776093
EpicM <santangelomike99@gmail.com>
2020-05-02 08:36:49 +0200

    rewrite jsonWriteTests after merge

--- src/test/java/io/github/oliviercailloux/teach_spreadsheets/json/JsonWriteTests.java
+++ src/test/java/io/github/oliviercailloux/teach_spreadsheets/json/JsonWriteTests.java
@@ -2,6 +2,7 @@ package io.github.oliviercailloux.teach_spreadsheets.json;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.io.InputStream;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.FileSystem;
@@ -27,23 +28,26 @@ public class JsonWriteTests {
 	@Test
 	void testWriteCoursesInAJsonFile() throws Exception {
 		URL url = CalcDataInitializerTests.class.getResource("Saisie_des_voeux_format simple.ods");
-		final Path infile = Path.of(url.toURI());
-		CalcData calcData = CalcData.getData(infile);
+		Path odsPath = Path.of(url.toURI());
 		
-		ImmutableSet<CoursePref> coursePrefs = calcData.getCoursePrefs();
-		HashSet<Course> courses = new HashSet<>();
-		for (CoursePref coursePref : coursePrefs) {
-			courses.add(coursePref.getCourse());
-		}
-		
-		try (FileSystem inMemoryFs = Jimfs.newFileSystem(Configuration.unix())) {
-			Path jsonPath = inMemoryFs.getPath("courses.json");
-			JsonWrite.writeCoursesInAJsonFile(jsonPath, courses);
-			
-			try (Jsonb jsonb = JsonbBuilder.create()) {
-				String expected = jsonb.toJson(courses.toArray());
-				String actual = Files.readString(jsonPath, StandardCharsets.UTF_8);
-				assertEquals(expected, actual);
+		try (InputStream odsStream = Files.newInputStream(odsPath)) {
+			CalcData calcData = CalcData.getData(odsStream);
+
+			ImmutableSet<CoursePref> coursePrefs = calcData.getCoursePrefs();
+			HashSet<Course> courses = new HashSet<>();
+			for (CoursePref coursePref : coursePrefs) {
+				courses.add(coursePref.getCourse());
+			}
+
+			try (FileSystem inMemoryFs = Jimfs.newFileSystem(Configuration.unix())) {
+				Path jsonPath = inMemoryFs.getPath("courses.json");
+				JsonWrite.writeCoursesInAJsonFile(jsonPath, courses);
+
+				try (Jsonb jsonb = JsonbBuilder.create()) {
+					String expected = jsonb.toJson(courses.toArray());
+					String actual = Files.readString(jsonPath, StandardCharsets.UTF_8);
+					assertEquals(expected, actual);
+				}
 			}
 		}
 	}
