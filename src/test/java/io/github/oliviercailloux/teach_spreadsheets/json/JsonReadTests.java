cbaa12d7db06a55d10502b86ac68743bd5d84d8d
Sarah-Elhelw <sarah_elhelw@hotmail.fr>
2020-05-03 10:21:39 +0200

    Comparing teachers and courses in unit tests bit by bit

--- src/test/java/io/github/oliviercailloux/teach_spreadsheets/json/JsonReadTests.java
+++ src/test/java/io/github/oliviercailloux/teach_spreadsheets/json/JsonReadTests.java
@@ -14,6 +14,9 @@ import javax.json.bind.JsonbException;
 import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.junit.jupiter.api.Test;
 
+import io.github.oliviercailloux.teach_spreadsheets.base.Course;
+import io.github.oliviercailloux.teach_spreadsheets.base.Teacher;
+
 public class JsonReadTests {
 
 	@Test
@@ -21,9 +24,23 @@ public class JsonReadTests {
 		URL resourceUrl = JsonRead.class.getResource("Courses.json");
 		final Path path = Path.of(resourceUrl.toURI());
 		final String textFile = Files.readString(path);
-		String actualIs = JsonRead.getSetOfCoursesInfo(textFile).toString();
-		String expectedIS = "[Course{name=PRE-RENTREE : Mathématiques, countGroupsTD=0, countGroupsCMTD=6, countGroupsTP=0, countGroupsCMTP=0, countGroupsCM=0, nbMinutesTD=0, nbMinutesCMTD=900, nbMinutesTP=0, nbMinutesCMTP=0, nbMinutesCM=0, studyYear=2016/2017, semester=1}, Course{name=Analyse 1, countGroupsTD=0, countGroupsCMTD=5, countGroupsTP=0, countGroupsCMTP=0, countGroupsCM=0, nbMinutesTD=0, nbMinutesCMTD=450, nbMinutesTP=0, nbMinutesCMTP=0, nbMinutesCM=0, studyYear=2016/2017, semester=1}]";
-		assertEquals(expectedIS, actualIs);
+		
+		Course actualCourse = JsonRead.getSetOfCoursesInfo(textFile).asList().get(1);
+		
+		assertEquals("Analyse 1", actualCourse.getName());
+		assertEquals(0, actualCourse.getCountGroupsTD());
+		assertEquals(5, actualCourse.getCountGroupsCMTD());
+		assertEquals(0, actualCourse.getCountGroupsTP());
+		assertEquals(0, actualCourse.getCountGroupsCMTP());
+		assertEquals(0, actualCourse.getCountGroupsCM());
+		assertEquals(0, actualCourse.getNbMinutesTD());
+		assertEquals(450, actualCourse.getNbMinutesCMTD());
+		assertEquals(0, actualCourse.getNbMinutesTP());
+		assertEquals(0, actualCourse.getNbMinutesCMTP());
+		assertEquals(0, actualCourse.getNbMinutesCM());
+		assertEquals("2016/2017", actualCourse.getStudyYear());
+		assertEquals(1, actualCourse.getSemester());
+		
 	}
 
 	/**
@@ -58,8 +75,20 @@ public class JsonReadTests {
 		
 		JsonRead.authentification(jsonLogin);
 		
-		String actualIs = JsonRead.getSetOfTeachersInfo("https://rof.testapi.dauphine.fr/ebx-dataservices/rest/data/v1/BpvRefRof/RefRof/root/Person").asList().get(1).toString();
-		String expectedIS = "Teacher{lastName=BRACI, firstName=LINA, address=, postCode=, city=, personalPhone=, mobilePhone=, personalEmail=, dauphineEmail=lina.braci@dauphine.fr, status=PROF.UNIV., dauphinePhoneNumber=, office=}";
-		assertEquals(expectedIS, actualIs);
+		Teacher actualTeacher = JsonRead.getSetOfTeachersInfo("https://rof.testapi.dauphine.fr/ebx-dataservices/rest/data/v1/BpvRefRof/RefRof/root/Person").asList().get(1);
+		
+		assertEquals("BRACI", actualTeacher.getLastName());
+		assertEquals("LINA", actualTeacher.getFirstName());
+		assertEquals("", actualTeacher.getAddress());
+		assertEquals("", actualTeacher.getPostCode());
+		assertEquals("", actualTeacher.getCity());
+		assertEquals("", actualTeacher.getPersonalPhone());
+		assertEquals("", actualTeacher.getMobilePhone());
+		assertEquals("", actualTeacher.getPersonalEmail());
+		assertEquals("lina.braci@dauphine.fr", actualTeacher.getDauphineEmail());
+		assertEquals("PROF.UNIV.", actualTeacher.getStatus());
+		assertEquals("", actualTeacher.getDauphinePhoneNumber());
+		assertEquals("", actualTeacher.getOffice());
+		
 	}
 }
