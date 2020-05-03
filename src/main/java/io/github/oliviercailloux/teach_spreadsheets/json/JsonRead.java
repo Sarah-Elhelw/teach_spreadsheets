93466ff1864dd24f2ae35219e442b40f5a5c0ffc
Sarah-Elhelw <sarah_elhelw@hotmail.fr>
2020-05-03 10:39:54 +0200

    Reformating and javadoc

--- src/main/java/io/github/oliviercailloux/teach_spreadsheets/json/JsonRead.java
+++ src/main/java/io/github/oliviercailloux/teach_spreadsheets/json/JsonRead.java
@@ -32,11 +32,12 @@ public class JsonRead {
 	/**
 	 * This method checks that the json String is only a single array.
 	 * 
-	 * @param textArray - the json text containing an array.
-	 *  
+	 * @param textArray - the json String.
+	 * 
 	 * @throws NullPointerException     if the parameter is null
 	 * @throws IllegalArgumentException if the parameter does not contain an array
-	 *                                  or contains several arrays.
+	 *                                  or contains several arrays or is not a
+	 *                                  single array.
 	 */
 	private static void checkFormatOfArray(String textArray) {
 		checkNotNull(textArray, "The String must not be null.");
@@ -44,15 +45,14 @@ public class JsonRead {
 		int rightSquareBracket = textArray.indexOf(']');
 		checkArgument(leftSquareBracket != -1 && rightSquareBracket != -1, "The parameter must be a json text containing an array.");
 		checkArgument(leftSquareBracket == textArray.lastIndexOf('[') && rightSquareBracket == textArray.lastIndexOf(']'), "The parameter must contain a single array.");
-		checkArgument(textArray.trim().equals(textArray.substring(leftSquareBracket, rightSquareBracket+1)), "The parameter should only be a single array.");
+		checkArgument(textArray.trim().equals(textArray.substring(leftSquareBracket, rightSquareBracket + 1)), "The parameter should only be a single array.");
 	}
-	
 	
 	/**
 	 * This method deserializes a json string into an ImmutableSet of courses. The
 	 * lines of code that deserialize the json string are inspired from
-	 * <a href="http://json-b.net/docs/user-guide.html">this website</a>.
-	 * The json string read must contain a single array.
+	 * <a href="http://json-b.net/docs/user-guide.html">this website</a>. The json
+	 * string read must contain a single array.
 	 * 
 	 * @param textFile - the json string containing a list of courses.
 	 * 
@@ -63,7 +63,10 @@ public class JsonRead {
 	public static ImmutableSet<Course> getSetOfCoursesInfo(String textFile) throws Exception {
 		checkFormatOfArray(textFile);
 		try (Jsonb jsonb = JsonbBuilder.create()) {
-			/** We first build each course to make sure they represent proper and acceptable Course objects :*/
+			/**
+			 * We first build each course to make sure they represent proper and acceptable
+			 * Course objects :
+			 */
 			List<Course.Builder> coursesB = jsonb.fromJson(textFile, new ArrayList<Course.Builder>() {
 				private static final long serialVersionUID = -7485196487129232750L;
 			}.getClass().getGenericSuperclass());
@@ -77,10 +80,11 @@ public class JsonRead {
 	}
 	
 	/**
-	 * This method connects to RefRof using a json String containing the user name and the password.
-	 * The json String must have the following structure: it contains the key "userName" whose value 
-	 * is the user name needed to get connected to RefRof and the key "password" whose value is the 
-	 * password needed to get connected to RefRof.
+	 * This method connects to RefRof using a json String containing the user name
+	 * and the password. The json String must have the following structure: it
+	 * contains the key "userName" whose value is the user name needed to get
+	 * connected to RefRof and the key "password" whose value is the password needed
+	 * to get connected to RefRof.
 	 * 
 	 * @param jsonLogin - the json String containing the login informations
 	 * 
@@ -105,22 +109,21 @@ public class JsonRead {
 
 	
 	/**
-	 * This method reads the content (which is just one line) of the RefRof page concerning the teachers ;
-	 * this content is in json. Then, it extracts from it the informations needed to build the set of of teachers.
+	 * This method reads the content (which is just one line) of the RefRof page
+	 * concerning the teachers ; this content is in json. Then, it extracts from it
+	 * the informations needed to build the set of of teachers.
 	 * 
-	 * @param httpAdress    - the web address to the RefRof page containing the
-	 *                      informations on teachers.
+	 * @param httpAdress - the web address to the RefRof page containing the
+	 *                   informations on teachers.
 	 * 
 	 * @return an ImmutableSet of teachers.
 	 * 
-	 * @throws Exception, thrown by close() if the resource cannot be closed.
 	 */
 	public static ImmutableSet<Teacher> getSetOfTeachersInfo(String httpAddress) {
 		Client client = ClientBuilder.newClient();
 		WebTarget t1 = client.target(httpAddress);
 		String content = t1.request(MediaType.TEXT_PLAIN).get(String.class);
 		client.close();
-		
 		
 		Set<Teacher> teachers = new LinkedHashSet<>();
 
