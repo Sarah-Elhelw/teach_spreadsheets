1f14a493b86554d42d7dad8da35918bf6bd26dbf
Sarah-Elhelw <sarah_elhelw@hotmail.fr>
2020-05-12 20:48:34 +0200

    Removing code's duplication in GlobalAssignment

--- src/main/java/io/github/oliviercailloux/teach_spreadsheets/write/GlobalAssignment.java
+++ src/main/java/io/github/oliviercailloux/teach_spreadsheets/write/GlobalAssignment.java
@@ -1,10 +1,12 @@
 package io.github.oliviercailloux.teach_spreadsheets.write;
 
+import java.util.LinkedHashSet;
 import java.util.Set;
 
 import org.odftoolkit.simple.SpreadsheetDocument;
 import org.odftoolkit.simple.table.Table;
 
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
 
 import io.github.oliviercailloux.teach_spreadsheets.base.Course;
@@ -14,6 +16,8 @@ import io.github.oliviercailloux.teach_spreadsheets.assignment.TeacherAssignment
 
 public class GlobalAssignment {
 
+	private static final ImmutableList<String> GROUPS = ImmutableList.of("CM", "CMTD", "CMTP", "TD", "TP");
+	
 	/**
 	 * These Strings are the positions in the Summarized Ods of the Year, Semester,
 	 * Course, Teacher's name and the various preferences and assignment.
@@ -53,6 +57,59 @@ public class GlobalAssignment {
 	}
 
 	/**
+	 * This method sets teachers' preferences for a given course group in an ods
+	 * document.
+	 * 
+	 * @param ods    - the ods document to complete
+	 * @param line   - the starting line in the ods document where to write
+	 * @param course - the given course
+	 * @param prefs  - a set of teachers' preferences for the given course. There
+	 *               should be no duplicates of teachers' preferences.
+	 * @param group  - the group of the given course
+	 * 
+	 * @return line - the updated index of line
+	 */
+	private static int setTeachersPreferencesForGroup(OdsHelper ods, int line, Course course, Set<CoursePref> prefs, String group, Set<TeacherAssignment> teachersAssigned) {
+		boolean courseHasTeacher = false;
+		if (course.getCountGroups(group) > 0) {
+
+			line++;
+			ods.setValueAt(group, line, 2);
+			ods.setValueAt(String.valueOf(course.getCountGroups(group)), line, 3);
+			ods.setValueAt(String.valueOf(course.getNbMinutes(group)), line, 4);
+
+			for (CoursePref p : prefs) {
+
+				if (course.getName().equals(p.getCourse().getName()) && !p.getPref(group).toString().equals("UNSPECIFIED") ) {
+					
+					courseHasTeacher = true;
+					ods.setValueAt(p.getTeacher().getFirstName(), line, 5);
+					ods.setValueAt(p.getTeacher().getLastName(), line, 6);
+					ods.setValueAt(p.getPref(group).toString(), line, 7);
+					
+					for (TeacherAssignment ta : teachersAssigned) {
+						if (p.getTeacher().getFirstName().equals(ta.getTeacher().getFirstName())
+								&& p.getTeacher().getLastName().equals(ta.getTeacher().getLastName())
+								&& ta.getCountGroups(group) != 0) {
+							ods.setValueAt(ta.getTeacher().getFirstName(), line, 8);
+							ods.setValueAt(ta.getTeacher().getLastName(), line, 9);
+
+						}
+					}
+					
+					line++;
+				}
+			}
+
+			if (!courseHasTeacher) {
+				line++;
+			}
+
+		}
+		return line;
+	}
+	
+	/**
 	 * This method creates a summarized Ods like FichierAgrege.pdf. For each course,
 	 * it writes all the teachers who want to teach the course, their preferences
 	 * and the possible assignment.
@@ -75,204 +132,24 @@ public class GlobalAssignment {
 		
 		headersToOds(summary);
 		int line = 3;
-		boolean courseHasTeacher = false; 
 
-		for (Course c : allCourses) {
+		for (Course course : allCourses) {
 
-			ods.setValueAt(c.getStudyYear(), line, 0);
-			ods.setValueAt(String.valueOf(c.getSemester()), line, 1);
-			ods.setValueAt(c.getName(), line, 2);
+			ods.setValueAt(course.getStudyYear(), line, 0);
+			ods.setValueAt(String.valueOf(course.getSemester()), line, 1);
+			ods.setValueAt(course.getName(), line, 2);
 			line++;
-			Set<TeacherAssignment> teachersAssigned;
+			Set<TeacherAssignment> teachersAssigned = new LinkedHashSet<>();
 
 			for (CourseAssignment ca : allCoursesAssigned) {
-				if (c.getName().equals(ca.getCourse().getName())) {
+				if (course.getName().equals(ca.getCourse().getName())) {
 					teachersAssigned = ca.getTeacherAssignments();
-
-					if (c.getCountGroupsCM() > 0) {
-
-						line++;
-						courseHasTeacher = false;
-						ods.setValueAt("CM", line, 2);
-						ods.setValueAt(String.valueOf(c.getCountGroupsCM()), line, 3);
-						ods.setValueAt(String.valueOf(c.getNbMinutesCM()), line, 4);
-
-						for (CoursePref p : prefs) {
-
-							if (c.getName().equals(p.getCourse().getName()) && !p.getPrefCM().toString().equals("UNSPECIFIED")) {
-								courseHasTeacher = true;
-								ods.setValueAt(p.getTeacher().getFirstName(), line, 5);
-								ods.setValueAt(p.getTeacher().getLastName(), line, 6);
-								ods.setValueAt(p.getPrefCM().toString(), line, 7);
-								
-
-								for (TeacherAssignment ta : teachersAssigned) {
-									if (p.getTeacher().getFirstName().equals(ta.getTeacher().getFirstName())
-											&& p.getTeacher().getLastName().equals(ta.getTeacher().getLastName())
-											&& ta.getCountGroupsCM() != 0) { 
-										ods.setValueAt(ta.getTeacher().getFirstName(), line, 8);
-										ods.setValueAt(ta.getTeacher().getLastName(), line, 9);
-
-									}
-								}
-
-								line++;
-							}
-
-						}
-
-						if (!courseHasTeacher) {
-							line++;
-						}
-
-					}
-
-					if (c.getCountGroupsCMTD() > 0) {
-
-						line++;
-						courseHasTeacher = false;
-						ods.setValueAt("CMTD", line, 2);
-						ods.setValueAt(String.valueOf(c.getCountGroupsCMTD()), line, 3);
-						ods.setValueAt(String.valueOf(c.getNbMinutesCMTD()), line, 4);
-
-						for (CoursePref p : prefs) {
-
-							if (c.getName().equals(p.getCourse().getName()) && !p.getPrefCMTD().toString().equals("UNSPECIFIED")) {
-								courseHasTeacher = true;
-								ods.setValueAt(p.getTeacher().getFirstName(), line, 5);
-								ods.setValueAt(p.getTeacher().getLastName(), line, 6);
-								ods.setValueAt(p.getPrefCMTD().toString(), line, 7);
-								
-
-								for (TeacherAssignment ta : teachersAssigned) {
-									if (p.getTeacher().getFirstName().equals(ta.getTeacher().getFirstName())
-											&& p.getTeacher().getLastName().equals(ta.getTeacher().getLastName())
-											&& ta.getCountGroupsCMTD() != 0) { 
-										ods.setValueAt(ta.getTeacher().getFirstName(), line, 8);
-										ods.setValueAt(ta.getTeacher().getLastName(), line, 9);
-
-									}
-								}
-
-								line++;
-							}
-						}
-
-						if (!courseHasTeacher) {
-							line++;
-						}
-
-					}
-
-					if (c.getCountGroupsCMTP() > 0) {
-
-						line++;
-						courseHasTeacher = false;
-						ods.setValueAt("CMTP", line, 2);
-						ods.setValueAt(String.valueOf(c.getCountGroupsCMTP()), line, 3);
-						ods.setValueAt(String.valueOf(c.getNbMinutesCMTP()), line, 4);
-
-						for (CoursePref p : prefs) {
-
-							if (c.getName().equals(p.getCourse().getName()) && !p.getPrefCMTP().toString().equals("UNSPECIFIED")) {
-								courseHasTeacher = true;
-								ods.setValueAt(p.getTeacher().getFirstName(), line, 5);
-								ods.setValueAt(p.getTeacher().getLastName(), line, 6);
-								ods.setValueAt(p.getPrefCMTP().toString(), line, 7);
-								
-
-								for (TeacherAssignment ta : teachersAssigned) {
-									if (p.getTeacher().getFirstName().equals(ta.getTeacher().getFirstName())
-											&& p.getTeacher().getLastName().equals(ta.getTeacher().getLastName())
-											&& ta.getCountGroupsCMTP() != 0) { 
-										ods.setValueAt(ta.getTeacher().getFirstName(), line, 8);
-										ods.setValueAt(ta.getTeacher().getLastName(), line, 9);
-
-									}
-								}
-
-								line++;
-							}
-						}
-
-						if (!courseHasTeacher) {
-							line++;
-						}
-
-					}
-
-					if (c.getCountGroupsTD() > 0) {
-
-						line++;
-						courseHasTeacher = false;
-						ods.setValueAt("TD", line, 2);
-						ods.setValueAt(String.valueOf(c.getCountGroupsTD()), line, 3);
-						ods.setValueAt(String.valueOf(c.getNbMinutesTD()), line, 4);
-
-						for (CoursePref p : prefs) {
-
-							if (c.getName().equals(p.getCourse().getName()) && !p.getPrefTD().toString().equals("UNSPECIFIED")) {
-								courseHasTeacher = true;
-								ods.setValueAt(p.getTeacher().getFirstName(), line, 5);
-								ods.setValueAt(p.getTeacher().getLastName(), line, 6);
-								ods.setValueAt(p.getPrefTD().toString(), line, 7);
-								
-
-								for (TeacherAssignment ta : teachersAssigned) {
-									if (p.getTeacher().getFirstName().equals(ta.getTeacher().getFirstName())
-											&& p.getTeacher().getLastName().equals(ta.getTeacher().getLastName())
-											&& ta.getCountGroupsTD() != 0) { 
-										ods.setValueAt(ta.getTeacher().getFirstName(), line, 8);
-										ods.setValueAt(ta.getTeacher().getLastName(), line, 9);
-
-									}
-								}
-
-								line++;
-							}
-						}
-
-						if (!courseHasTeacher) {
-							line++;
-						}
-
-					}
-
-					if (c.getCountGroupsTP() > 0) {
-
-						line++;
-						courseHasTeacher = false;
-						ods.setValueAt("TP", line, 2);
-						ods.setValueAt(String.valueOf(c.getCountGroupsTP()), line, 3);
-						ods.setValueAt(String.valueOf(c.getNbMinutesTP()), line, 4);
-
-						for (CoursePref p : prefs) {
-
-							if (c.getName().equals(p.getCourse().getName()) && !p.getPrefTP().toString().equals("UNSPECIFIED")) {
-								courseHasTeacher = true;
-								ods.setValueAt(p.getTeacher().getFirstName(), line, 5);
-								ods.setValueAt(p.getTeacher().getLastName(), line, 6);
-								ods.setValueAt(p.getPrefTP().toString(), line, 7);
-								
-
-								for (TeacherAssignment ta : teachersAssigned) {
-									if (p.getTeacher().getFirstName().equals(ta.getTeacher().getFirstName())
-											&& p.getTeacher().getLastName().equals(ta.getTeacher().getLastName())
-											&& ta.getCountGroupsTP() != 0) { 
-										ods.setValueAt(ta.getTeacher().getFirstName(), line, 8);
-										ods.setValueAt(ta.getTeacher().getLastName(), line, 9);
-									}
-								}
-
-								line++;
-							}
-						}
-
-						if (!courseHasTeacher) {
-							line++;
-						}
-					}
+					break;
 				}
+			}
+			
+			for (String group : GROUPS) {
+				line = setTeachersPreferencesForGroup(ods, line, course, prefs, group, teachersAssigned);
 			}
 		}
 
