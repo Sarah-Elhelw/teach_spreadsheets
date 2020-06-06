ded0cc8eff777c3f683344ffe8551b8aa4808c5e
inesdraredja <ines.draredja@dauphine.eu>
2020-06-06 21:27:25 +0200

    logger

---
+++ src/main/java/io/github/oliviercailloux/teach_spreadsheets/assignment/logger.java
@@ -0,0 +1,32 @@
+package io.github.oliviercailloux.teach_spreadsheets.assignment;
+import java.util.logging.Level;
+
+import java.util.logging.Logger;
+ 
+
+
+public class logger {
+
+ 
+
+/**
+
+* @param args
+
+*/
+
+public static void main(String[] args) {
+
+Logger logger = Logger.getLogger("logger");
+
+logger.log(Level.INFO, "Test de logger");
+
+ 
+
+ 
+
+}
+
+ 
+
+}
