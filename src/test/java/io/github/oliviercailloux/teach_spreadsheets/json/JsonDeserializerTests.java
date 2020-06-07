6589d3a98b8519816624d44cb3cc7d164b076ba4
Sarah-Elhelw <sarah_elhelw@hotmail.fr>
2020-06-07 14:56:07 +0200

    Removing warning Potential null pointer access

--- src/test/java/io/github/oliviercailloux/teach_spreadsheets/json/JsonDeserializerTests.java
+++ src/test/java/io/github/oliviercailloux/teach_spreadsheets/json/JsonDeserializerTests.java
@@ -13,7 +13,6 @@ import javax.json.bind.JsonbException;
 import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.junit.jupiter.api.Test;
 
-import static com.google.common.base.Verify.verify;
 import com.google.common.collect.ImmutableSet;
 
 import io.github.oliviercailloux.teach_spreadsheets.base.Course;
@@ -23,27 +22,29 @@ public class JsonDeserializerTests {
 	@Test
 	void testToCourses() throws Exception {
 		URL resourceUrl = JsonDeserializer.class.getResource("Courses.json");
-		verify(resourceUrl != null);
-		final Path path = Path.of(resourceUrl.toURI());
+		if(resourceUrl != null) {
+			final Path path = Path.of(resourceUrl.toURI());
 
-		ImmutableSet<Course> courses = JsonDeserializer.toCourses(Files.readString(path));
-		assertTrue(courses.size() == 2);
+			ImmutableSet<Course> courses = JsonDeserializer.toCourses(Files.readString(path));
+			assertTrue(courses.size() == 2);
 
-		Course actualCourse = courses.asList().get(1);
-		assertEquals("Analyse 1", actualCourse.getName());
-		assertEquals(0, actualCourse.getCountGroupsTD());
-		assertEquals(5, actualCourse.getCountGroupsCMTD());
-		assertEquals(0, actualCourse.getCountGroupsTP());
-		assertEquals(0, actualCourse.getCountGroupsCMTP());
-		assertEquals(0, actualCourse.getCountGroupsCM());
-		assertEquals(0, actualCourse.getNbMinutesTD());
-		assertEquals(450, actualCourse.getNbMinutesCMTD());
-		assertEquals(0, actualCourse.getNbMinutesTP());
-		assertEquals(0, actualCourse.getNbMinutesCMTP());
-		assertEquals(0, actualCourse.getNbMinutesCM());
-		assertEquals(2016, actualCourse.getStudyYear());
-		assertEquals("DE1", actualCourse.getStudyLevel());
-		assertEquals(1, actualCourse.getSemester());
+			Course actualCourse = courses.asList().get(1);
+			assertEquals("Analyse 1", actualCourse.getName());
+			assertEquals(0, actualCourse.getCountGroupsTD());
+			assertEquals(5, actualCourse.getCountGroupsCMTD());
+			assertEquals(0, actualCourse.getCountGroupsTP());
+			assertEquals(0, actualCourse.getCountGroupsCMTP());
+			assertEquals(0, actualCourse.getCountGroupsCM());
+			assertEquals(0, actualCourse.getNbMinutesTD());
+			assertEquals(450, actualCourse.getNbMinutesCMTD());
+			assertEquals(0, actualCourse.getNbMinutesTP());
+			assertEquals(0, actualCourse.getNbMinutesCMTP());
+			assertEquals(0, actualCourse.getNbMinutesCM());
+			assertEquals(2016, actualCourse.getStudyYear());
+			assertEquals("DE1", actualCourse.getStudyLevel());
+			assertEquals(1, actualCourse.getSemester());
+		}
+		
 
 	}
 
@@ -54,12 +55,14 @@ public class JsonDeserializerTests {
 	@Test
 	void testToCoursesWithNullName() throws Exception {
 		URL resourceUrl = JsonDeserializer.class.getResource("CoursesWithNullName.json");
-		verify(resourceUrl != null);
-		final Path path = Path.of(resourceUrl.toURI());
-		Throwable exception = assertThrows(JsonbException.class, () -> {
-			JsonDeserializer.toCourses(Files.readString(path));
-		});
-		assertEquals("String must not be null.", ExceptionUtils.getRootCause(exception).getMessage());
+		if(resourceUrl != null) {
+			final Path path = Path.of(resourceUrl.toURI());
+			Throwable exception = assertThrows(JsonbException.class, () -> {
+				JsonDeserializer.toCourses(Files.readString(path));
+			});
+			assertEquals("String must not be null.", ExceptionUtils.getRootCause(exception).getMessage());
+		}
+		
 	}
 
 }
--- src/test/java/io/github/oliviercailloux/teach_spreadsheets/json/JsonSerializerTests.java
+++ src/test/java/io/github/oliviercailloux/teach_spreadsheets/json/JsonSerializerTests.java
@@ -1,6 +1,5 @@
 package io.github.oliviercailloux.teach_spreadsheets.json;
 
-import static com.google.common.base.Verify.verify;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.io.InputStream;
@@ -28,42 +27,42 @@ public class JsonSerializerTests {
 	@Test
 	void testSerializeSet() throws Exception {
 		URL url = CalcDataInitializerTests.class.getResource("Saisie_des_voeux_format simple.ods");
-		verify(url != null);
+		if(url != null) {
+			try (InputStream odsStream = url.openStream()) {
+				CalcData calcData = CalcData.getData(odsStream);
 
-		try (InputStream odsStream = url.openStream()) {
-			CalcData calcData = CalcData.getData(odsStream);
+				ImmutableSet<CoursePref> coursePrefs = calcData.getCoursePrefs();
+				Set<Course> courses = new LinkedHashSet<>();
+				for (CoursePref coursePref : coursePrefs) {
+					courses.add(coursePref.getCourse());
+				}
 
-			ImmutableSet<CoursePref> coursePrefs = calcData.getCoursePrefs();
-			Set<Course> courses = new LinkedHashSet<>();
-			for (CoursePref coursePref : coursePrefs) {
-				courses.add(coursePref.getCourse());
-			}
+				String serializedSet = JsonSerializer.serializeSet(courses);
 
-			String serializedSet = JsonSerializer.serializeSet(courses);
+				try (JsonReader jr = Json.createReader(new StringReader(serializedSet))) {
+					JsonArray ja = jr.readArray();
 
-			try (JsonReader jr = Json.createReader(new StringReader(serializedSet))) {
-				JsonArray ja = jr.readArray();
+					assertEquals(courses.size(), ja.size());
 
-				assertEquals(courses.size(), ja.size());
+					JsonObject actual = ja.getJsonObject(0);
+					Course expected = new ArrayList<Course>(courses).get(0);
 
-				JsonObject actual = ja.getJsonObject(0);
-				Course expected = new ArrayList<Course>(courses).get(0);
+					assertEquals(expected.getName(), actual.getString("name"));
+					assertEquals(expected.getStudyLevel(), actual.getString("studyLevel"));
+					assertEquals(expected.getStudyYear(), actual.getInt("studyYear"));
+					assertEquals(expected.getSemester(), actual.getInt("semester"));
+					assertEquals(expected.getCountGroupsTD(), actual.getInt("countGroupsTD"));
+					assertEquals(expected.getCountGroupsTP(), actual.getInt("countGroupsTP"));
+					assertEquals(expected.getCountGroupsCMTD(), actual.getInt("countGroupsCMTD"));
+					assertEquals(expected.getCountGroupsCMTP(), actual.getInt("countGroupsCMTP"));
+					assertEquals(expected.getCountGroupsCM(), actual.getInt("countGroupsCM"));
+					assertEquals(expected.getNbMinutesTD(), actual.getInt("nbMinutesTD"));
+					assertEquals(expected.getNbMinutesTP(), actual.getInt("nbMinutesTP"));
+					assertEquals(expected.getNbMinutesCMTD(), actual.getInt("nbMinutesCMTD"));
+					assertEquals(expected.getNbMinutesCMTP(), actual.getInt("nbMinutesCMTP"));
+					assertEquals(expected.getNbMinutesCM(), actual.getInt("nbMinutesCM"));
 
-				assertEquals(expected.getName(), actual.getString("name"));
-				assertEquals(expected.getStudyLevel(), actual.getString("studyLevel"));
-				assertEquals(expected.getStudyYear(), actual.getInt("studyYear"));
-				assertEquals(expected.getSemester(), actual.getInt("semester"));
-				assertEquals(expected.getCountGroupsTD(), actual.getInt("countGroupsTD"));
-				assertEquals(expected.getCountGroupsTP(), actual.getInt("countGroupsTP"));
-				assertEquals(expected.getCountGroupsCMTD(), actual.getInt("countGroupsCMTD"));
-				assertEquals(expected.getCountGroupsCMTP(), actual.getInt("countGroupsCMTP"));
-				assertEquals(expected.getCountGroupsCM(), actual.getInt("countGroupsCM"));
-				assertEquals(expected.getNbMinutesTD(), actual.getInt("nbMinutesTD"));
-				assertEquals(expected.getNbMinutesTP(), actual.getInt("nbMinutesTP"));
-				assertEquals(expected.getNbMinutesCMTD(), actual.getInt("nbMinutesCMTD"));
-				assertEquals(expected.getNbMinutesCMTP(), actual.getInt("nbMinutesCMTP"));
-				assertEquals(expected.getNbMinutesCM(), actual.getInt("nbMinutesCM"));
-
+				}
 			}
 		}
 	}
