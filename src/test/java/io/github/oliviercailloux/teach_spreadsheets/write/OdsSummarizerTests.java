49046677266f5d8f80b57f38c3c36a470c153820
hedi-guedidi <hedi.guedidi@dauphine.eu>
2020-06-17 17:26:07 +0200

    adding doc,loggers,and correcting code

--- src/test/java/io/github/oliviercailloux/teach_spreadsheets/write/OdsSummarizerTests.java
+++ src/test/java/io/github/oliviercailloux/teach_spreadsheets/write/OdsSummarizerTests.java
@@ -5,6 +5,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 import org.odftoolkit.simple.SpreadsheetDocument;
 import org.odftoolkit.simple.table.Table;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.google.common.collect.ImmutableSet;
 
@@ -14,6 +16,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.LinkedHashSet;
 import java.util.Set;
+import java.util.concurrent.TimeUnit;
 
 import io.github.oliviercailloux.teach_spreadsheets.assignment.CourseAssignment;
 import io.github.oliviercailloux.teach_spreadsheets.assignment.TeacherAssignment;
@@ -23,10 +26,18 @@ import io.github.oliviercailloux.teach_spreadsheets.base.Preference;
 import io.github.oliviercailloux.teach_spreadsheets.base.Teacher;
 
 public class OdsSummarizerTests {
+	private final static Logger LOGGER = LoggerFactory.getLogger(OdsSummarizerTests.class);
 
 	@Test
+	/**
+	 * this test creates 2 assignments and writes in memory.
+	 * 
+	 * @throws Exception
+	 */
 	void testCreateSummary() throws Exception {
-
+		/**
+		 * Creation of 2 test assignments.
+		 */
 		Course course1 = Course.Builder.newInstance().setName("testcourse1").setStudyYear(2016).setStudyLevel("DE1")
 				.setSemester(1).setCountGroupsCM(3).setCountGroupsTD(4).setNbMinutesCM(60).setNbMinutesTD(60).build();
 		Course course2 = Course.Builder.newInstance().setName("testcourse2").setStudyYear(2016).setStudyLevel("DE1")
@@ -68,16 +79,26 @@ public class OdsSummarizerTests {
 		Set<CourseAssignment> allCoursesAssigned = new LinkedHashSet<>();
 		allCoursesAssigned.add(courseAssignment1);
 		allCoursesAssigned.add(courseAssignment2);
-
+		/**
+		 * Ods creation and writing.
+		 */
 		OdsSummarizer ods = OdsSummarizer.newInstance(courses);
 		ods.addPrefs(prefs);
+		long startTimeAddingAssignement = System.nanoTime();
+		LOGGER.info("2 assignments : start assignment and write");
 		ods.setAllCoursesAssigned(allCoursesAssigned);
-
+		LOGGER.info("2 assignments : stop assignment");
+		long startTimeWriting = System.nanoTime();
 		URL resourceUrl = OdsSummarizer.class.getResource("OdsSummarizer.ods");
 		try (InputStream stream = resourceUrl.openStream();
 				SpreadsheetDocument document = SpreadsheetDocument.loadDocument(stream);
 				SpreadsheetDocument documentCreated = ods.createSummary()) {
-
+			LOGGER.info("2 assignments : stop writing in memory");
+			long elapsedNanosWriteAndAssign = System.nanoTime() - startTimeAddingAssignement;
+			long elapsedNanosWrite = System.nanoTime() - startTimeWriting;
+			LOGGER.info("2 assignements : Time to write and assign : "
+					+ TimeUnit.NANOSECONDS.toSeconds(elapsedNanosWriteAndAssign) + "s" + " " + "Time to write : "
+					+ TimeUnit.NANOSECONDS.toSeconds(elapsedNanosWrite) + "s");
 			Table tableCreated = documentCreated.getTableByName("Summary");
 			Table table = document.getTableByName("Summary");
 
@@ -91,57 +112,19 @@ public class OdsSummarizerTests {
 	}
 
 	@Test
-	void testWritingSummaryAndAssignmentPerTeacher100() throws Exception {
-		Set<Course> courses = new LinkedHashSet<>();
-		Set<CoursePref> prefs = new LinkedHashSet<>();
-		
-		ImmutableSet<CourseAssignment> allCoursesAssigned=ImmutableSet.of();
-
-		for (int i = 1; i <= 100; i++) {
-			Course course = Course.Builder.newInstance().setName("testcourse" + i).setStudyYear(2016)
-					.setStudyLevel("DE1").setSemester(1).setCountGroupsCM(3).setCountGroupsTD(4).setNbMinutesCM(60)
-					.setNbMinutesTD(60).build();
-			courses.add(course);
-			Teacher teacher = Teacher.Builder.newInstance().setFirstName("teacher" + i + "FirstName")
-					.setLastName("teacher" + i + "LastName").build();
-			prefs.add(CoursePref.Builder.newInstance(course, teacher).setPrefCM(Preference.A).setPrefTD(Preference.B)
-					.build());
-
-			TeacherAssignment teacherAssignment = TeacherAssignment.Builder.newInstance(course, teacher)
-					.setCountGroupsTD(1).build();
-			CourseAssignment.Builder courseAssignmentBuilder = CourseAssignment.Builder.newInstance(course);
-			courseAssignmentBuilder.addTeacherAssignment(teacherAssignment);
-
-			CourseAssignment courseAssignment = courseAssignmentBuilder.build();
-
-			allCoursesAssigned = ImmutableSet.of(courseAssignment);
-			try (SpreadsheetDocument documentCreated = AssignmentPerTeacher.createAssignmentPerTeacher(teacher,
-					allCoursesAssigned)) {
-				if (!Files.exists(Path.of("output"))) {
-					Files.createDirectory(Path.of("output"));
-				}
-				documentCreated.save("output//teacherAssignment" + i + ".ods");
-			}
-		}
-
-		OdsSummarizer ods = OdsSummarizer.newInstance(courses);
-		ods.addPrefs(prefs);
-		ods.setAllCoursesAssigned(allCoursesAssigned);
-		try (SpreadsheetDocument documentCreated = ods.createSummary()) {
-			if (!Files.exists(Path.of("output"))) {
-				Files.createDirectory(Path.of("output"));
-			}
-			documentCreated.save("output//testOdsSummary100.ods");
-		}
-	}
-
-	@Test
+	/**
+	 * this test creates 100 assignments and writes on disk.
+	 * 
+	 * @throws Exception
+	 */
 	void testWriting100Summary() throws Exception {
 		Set<Course> courses = new LinkedHashSet<>();
 		Set<CoursePref> prefs = new LinkedHashSet<>();
-		
-		ImmutableSet<CourseAssignment> allCoursesAssigned=ImmutableSet.of();
 
+		Set<CourseAssignment> allCoursesAssigned = new LinkedHashSet<>();
+		/**
+		 * Creation of the 100 test assignments.
+		 */
 		for (int i = 1; i <= 100; i++) {
 			Course course = Course.Builder.newInstance().setName("testcourse" + i).setStudyYear(2016)
 					.setStudyLevel("DE1").setSemester(1).setCountGroupsCM(3).setCountGroupsTD(4).setNbMinutesCM(60)
@@ -156,19 +139,30 @@ public class OdsSummarizerTests {
 			CourseAssignment.Builder courseAssignmentBuilder = CourseAssignment.Builder.newInstance(course);
 			courseAssignmentBuilder.addTeacherAssignment(teacherAssignment);
 			CourseAssignment courseAssignment = courseAssignmentBuilder.build();
+			allCoursesAssigned.add(courseAssignment);
 
 		}
-		
-		// allCoursesAssigned = ImmutableSet.of(courseAssignment);
-
+		/**
+		 * Ods creation and writing.
+		 */
 		OdsSummarizer ods = OdsSummarizer.newInstance(courses);
 		ods.addPrefs(prefs);
-		ods.setAllCoursesAssigned(allCoursesAssigned);
+		long startTimeAddingAssignement = System.nanoTime();
+		LOGGER.info("100 assignments : start assignment and write");
+		ods.setAllCoursesAssigned(ImmutableSet.copyOf(allCoursesAssigned));
+		LOGGER.info("100 assignments : stop assignment");
+		long startTimeWriting = System.nanoTime();
 		try (SpreadsheetDocument documentCreated = ods.createSummary()) {
 			if (!Files.exists(Path.of("output"))) {
 				Files.createDirectory(Path.of("output"));
 			}
 			documentCreated.save("output//testWriting100.ods");
+			LOGGER.info("100 assignments : stop writing on disk");
 		}
+		long elapsedNanosWriteAndAssign = System.nanoTime() - startTimeAddingAssignement;
+		long elapsedNanosWrite = System.nanoTime() - startTimeWriting;
+		LOGGER.info("100 affectations : Time to write and assign : "
+				+ TimeUnit.NANOSECONDS.toSeconds(elapsedNanosWriteAndAssign) + "s" + " " + "Time to write : "
+				+ TimeUnit.NANOSECONDS.toSeconds(elapsedNanosWrite) + "s");
 	}
 }
