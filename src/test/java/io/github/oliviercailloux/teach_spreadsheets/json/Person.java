01583447e12fbb143f4251a0bc5bd94e5adec3a3
Sarah-Elhelw <sarah_elhelw@hotmail.fr>
2020-06-22 23:35:36 +0200

    Responding to all the PR comment

--- src/test/java/io/github/oliviercailloux/teach_spreadsheets/json/Person.java
+++ src/test/java/io/github/oliviercailloux/teach_spreadsheets/json/Person.java
@@ -1,26 +1,17 @@
 package io.github.oliviercailloux.teach_spreadsheets.json;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Objects;
 
-import javax.json.bind.Jsonb;
-import javax.json.bind.JsonbBuilder;
 import javax.json.bind.annotation.JsonbCreator;
 
 import com.google.common.base.MoreObjects;
-import com.google.common.base.VerifyException;
-import com.google.common.collect.ImmutableList;
 
-import static com.google.common.base.Preconditions.checkNotNull;
 
 public class Person {
-	private String firstName;
-	private String lastName;
+	private String name;
 
 	private Person() {
-		firstName = "";
-		lastName = "";
+		name = "";
 	}
 
 	@JsonbCreator
@@ -28,49 +19,12 @@ public class Person {
 		return new Person();
 	}
 
-	/**
-	 * This method deserializes a json array string into an ImmutableList of
-	 * persons. The lines of code that deserialize the json string are inspired from
-	 * <a href="http://json-b.net/docs/user-guide.html">this website</a>.
-	 * 
-	 * @param json - the json array string.
-	 * 
-	 * @return an ImmutableList of persons.
-	 * 
-	 * @throws RuntimeException if any unexpected error(s) occur(s) during
-	 *                          deserialization.
-	 * @throws VerifyException  if the conversion failed
-	 * 
-	 */
-	public static ImmutableList<Person> toPersons(String json) {
-		checkNotNull(json, "The String must not be null.");
-		try (Jsonb jsonb = JsonbBuilder.create()) {
-			List<Person> list = jsonb.fromJson(json, new ArrayList<Person>() {
-				private static final long serialVersionUID = -7485196487128234751L;
-			}.getClass().getGenericSuperclass());
-			return ImmutableList.copyOf(list);
-		} catch (RuntimeException re) {
-			throw re;
-		} catch (Exception e) {
-			throw new VerifyException(e);
-		}
+	public String getName() {
+		return name;
 	}
 
-	public String getFirstName() {
-		return firstName;
-	}
-
-	public String getLastName() {
-		return lastName;
-	}
-
-	public Person setFirstName(String firstName) {
-		this.firstName = firstName;
-		return this;
-	}
-
-	public Person setLastName(String lastName) {
-		this.lastName = lastName;
+	public Person setName(String name) {
+		this.name = name;
 		return this;
 	}
 
@@ -84,17 +38,17 @@ public class Person {
 		}
 		Person p2 = (Person) o2;
 
-		return firstName.equals(p2.firstName) && lastName.equals(p2.lastName);
+		return name.equals(p2.name);
 	}
 
 	@Override
 	public int hashCode() {
-		return Objects.hash(firstName, lastName);
+		return Objects.hash(name);
 	}
 
 	@Override
 	public String toString() {
-		return MoreObjects.toStringHelper(this).add("firstName", firstName).add("lastName", lastName).toString();
+		return MoreObjects.toStringHelper(this).add("name", name).toString();
 	}
 
 }
--- src/test/java/io/github/oliviercailloux/teach_spreadsheets/json/PersonTests.java
+++ src/test/java/io/github/oliviercailloux/teach_spreadsheets/json/PersonTests.java
@@ -1,45 +1,94 @@
 package io.github.oliviercailloux.teach_spreadsheets.json;
 
+import static com.google.common.base.Preconditions.checkNotNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import java.net.URL;
-import java.nio.file.Files;
-import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.List;
+
+import javax.json.bind.Jsonb;
+import javax.json.bind.JsonbBuilder;
+import javax.json.stream.JsonParsingException;
 
 import org.junit.jupiter.api.Test;
 
+import com.google.common.collect.ImmutableList;
+
 public class PersonTests {
+	/**
+	 * This method deserializes a json array string into an ImmutableList of
+	 * persons. The lines of code that deserialize the json string are inspired from
+	 * <a href="http://json-b.net/docs/user-guide.html">this website</a>.
+	 * 
+	 * @param json - the json array string.
+	 * 
+	 * @return an ImmutableList of persons.
+	 * 
+	 */
+	private static ImmutableList<Person> toPersons(String json) throws Exception {
+		checkNotNull(json, "The String must not be null.");
+		try (Jsonb jsonb = JsonbBuilder.create()) {
+			List<Person> list = jsonb.fromJson(json, new ArrayList<Person>() {
+				private static final long serialVersionUID = -7485196487128234751L;
+			}.getClass().getGenericSuperclass());
+			return ImmutableList.copyOf(list);
+		}
+	}
+
 	@Test
 	void testToPersons() throws Exception {
 
-		/** CASE 1 : Deserialization with a wrongly formatted json file: */
+		/** CASE 1 : Deserialization with a json that is not a json array: */
 
-		URL resourceUrl1 = Person.class.getResource("WrongFormat.json");
-		final Path path1 = Path.of(resourceUrl1.toURI());
-		String json1 = Files.readString(path1);
+		String json1 = "{\"persons\":[{\"firstName\": \"John\",\"lastName\": \"Doe\"}]}";
 
-		List<Person> list1 = Person.toPersons(json1);
+		List<Person> list1 = PersonTests.toPersons(json1);
 
 		/** The deserialization process returns an empty list instead of failing fast: */
 		assertEquals(List.of(), list1);
 
-		/** CASE 2 : Control: */
+		/** CASE 2 : Control. Deserialization with a json array: */
 
-		URL resourceUrl2 = Person.class.getResource("RightFormat.json");
-		final Path path2 = Path.of(resourceUrl2.toURI());
-		String json2 = Files.readString(path2);
+		String json2 = "[{\"name\": \"John Doe\"}]";
 
-		List<Person> list2 = Person.toPersons(json2);
+		List<Person> list2 = PersonTests.toPersons(json2);
 
 		/** The deserialization process works properly: */
 
 		assertTrue(list2.size() == 1);
 
 		Person actual = list2.get(0);
-		Person expected = Person.newInstance().setFirstName("John").setLastName("Doe");
+		Person expected = Person.newInstance().setName("John Doe");
 		assertEquals(expected, actual);
+		
+		/** CASE 3 : Deserialization process fails fast if the json is wrongly formatted: */
+		
+		/** When the closing square bracket is missing in the json array: */
+		String json3 = "[{\"name\": \"John Doe\"}";
+		assertThrows(JsonParsingException.class, () -> {
+			PersonTests.toPersons(json3);
+		});
+
+		/** When the closing curly bracket is missing in the json array: */
+		String json4 = "[{\"name\": \"John Doe\"]";
+		assertThrows(JsonParsingException.class, () -> {
+			PersonTests.toPersons(json4);
+		});
+		
+		/** When the closing square bracket is missing in the json object: */
+		String json5 = "{\"persons\":[{\"firstName\": \"John\",\"lastName\": \"Doe\"}}";
+		assertThrows(JsonParsingException.class, () -> {
+			PersonTests.toPersons(json5);
+		});
+		
+		/** When the closing curly bracket is missing in the json object: */
+		String json6 = "{\"persons\":[{\"firstName\": \"John\",\"lastName\": \"Doe\"}]";
+		assertThrows(JsonParsingException.class, () -> {
+			PersonTests.toPersons(json6);
+		});
+
 	}
 
 }
